496f5c5c5dffbadc4847737368203205439ccd93
zeooon <zeooon@ya.ru>
2020-11-28 20:52:55 +0700

    My commit

--- app/src/main/java/com/zymixx/test_sql/MainActivity.java
+++ app/src/main/java/com/zymixx/test_sql/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -26,19 +27,27 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textForDB);
         mySQL = new MySQL(this);
 
+        Log.e("XLog", "My changes 1");
+        Log.e("XLog", "My changes 2");
+
         try {
             SQLiteDatabase db = mySQL.getWritableDatabase();
             db.execSQL("CREATE TABLE my_DB (id INTEGER primary key autoincrement, " +
                     "text varchar (250), titleNote varchar (22) DEFAULT 'Title', createTime varchar (24))");
             db.execSQL("CREATE TABLE config_DB (name VARCHAR (30), corent INTEGER)");
+            Log.e("XLog", "My changes 3");
             db.execSQL("CREATE TABLE recycle_bin_DB (id INTEGER primary key autoincrement, " +
                     "text varchar (250), titleNote varchar (22) DEFAULT 'Title', createTime varchar (24)) ");
             db.close();
         } catch (Exception ex) {
-            ex.printStackTrace();
+            Log.e("XLog", "My changes 4");
+            ex.printStackTrace();   //  TODO Так не делай. Дог будет не в потоке Error а в потоке Info/
+            Log.e("XLog", "Error", ex); // TODO Лучше так.
+            ControllerLog.error(ex);    // TODO Так идеально, если у тебя отдельный класс отвечает за логирование. Там сможешь кешировать логи и выводить их на экран приложения или отправлять на сервер например.
         }
 
         try {
+            Log.e("XLog", "My changes 5");
             SQLiteDatabase db = mySQL.getWritableDatabase();
             db.execSQL("DROP TABLE my_DB");
             db.execSQL("DROP TABLE config_DB");
